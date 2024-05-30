33d31c2892ba8bd5083921158955d18a0ebfe4e4
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-30 18:28:31 +0530

    update

--- 03_BinaryNumer/1_binaryFunct.py
+++ 03_BinaryNumer/1_binaryFunct.py
@@ -3,3 +3,5 @@
 a = 10
 
 print(format(a, 'b'))   # format(a, 'b') a represent variable name and 'b' stands for binary
+
+print(bin(a))   # output : 0b1010  ---- This is the another way to convert number into binary form
