9942c529141a3b0d7e97a4a6e30b16c4916377e0
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-30 18:31:14 +0530

    update

--- 03_BinaryNumer/1_binaryFunct.py
+++ 03_BinaryNumer/1_binaryFunct.py
@@ -5,3 +5,5 @@ a = 10
 print(format(a, 'b'))   # format(a, 'b') a represent variable name and 'b' stands for binary
 
 print(bin(a))   # output : 0b1010  ---- This is the another way to convert number into binary form
+
+print(a.bit_length())       # Output: 4 "bit_length()" function used to see the length of binary number
