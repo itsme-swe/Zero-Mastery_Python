ec5ca2b42b447af30a0a564d46f54ead72b67269
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-31 18:14:18 +0530

    update

---
+++ 08_StringMethods/12_fstringMethod.py
@@ -0,0 +1,7 @@
+name = 'Harsh'
+age = 32
+salary = "$2000"
+
+txt = f"{name} is earning {salary} salary per month and his age is {age}"
+
+print(txt)  # output: Harsh is earning $2000 salary per month and his age is 32
