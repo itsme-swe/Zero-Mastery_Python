bfd0570a0ffa123af08ac547a69be0b0c8a8eb2d
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-31 17:06:37 +0530

    update

---
+++ 08_StringMethods/4_countMethod.py
@@ -0,0 +1,11 @@
+fruits = "I am planting apples, oranges and green apples"
+
+print(fruits.count('apples'))       # Output: 2 is the number of items apple come in string fruits.
+
+print(fruits.count('water'))        # Output: 0 if the value is not present in string  
+
+'''
+🔸count() method returns the number of times a specified value appears in the string.
+
+   Syntax --  string.count(valToSearch, startIndex, endIndex)
+'''
