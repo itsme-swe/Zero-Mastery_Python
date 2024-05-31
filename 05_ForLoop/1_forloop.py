1d01f100a7afcdf57523c928fad2133f7303242b
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-31 11:17:13 +0530

    update

---
+++ 05_ForLoop/1_forloop.py
@@ -0,0 +1,9 @@
+msg = "Hello"
+
+for x in msg:
+    print(x, end = ' ')     # Output: H e l l o
+
+print()
+
+for x in range(1, 11):
+    print(x)
---
+++ 05_ForLoop/2_printTable.py
@@ -0,0 +1,6 @@
+# Print the table using for loop
+
+n = int(input('Enter the number: '))
+
+for count in range(1, 11):
+    print(n, '*', count, '=', n * count)
