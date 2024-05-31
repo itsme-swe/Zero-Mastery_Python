4bc37add8489e583377c36ca070f72ada7d78079
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-31 22:23:17 +0530

    update

---
+++ 09_FormattedPrinting/1_ASCII.py
@@ -0,0 +1,13 @@
+s = 'A'
+
+v = 'a'
+
+print(ord(s))   # output: 65 ASCII value of "A"
+
+print(ord(v))   # output: 97 ASCII value of "a"
+
+print(chr(81))  # output: Q ASCII char of 81
+
+'''
+🔸ord() function is used to know the ASCII value of any character
+'''
