340c8dfbd0dcf971315f1f70cddf08757b263ad0
Harsh Mehra <me.harshmehra07@gmail.com>
2024-05-31 21:48:57 +0530

    update

---
+++ 08_StringMethods/14_casefoldMethod.py
@@ -0,0 +1,9 @@
+msg = 'Welcome to The TECH WorLD'
+
+newMsg = msg.casefold()
+
+print(newMsg)   # Output: welcome to the tech world
+
+'''
+🔸casefold() method returns a string where all the characters are lower case.
+'''
