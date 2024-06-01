014056de3cf9c22f33c7a241dbe9f3dcb37d07a3
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-01 12:56:52 +0530

    update

---
+++ 10_List/2_operatorList.py
@@ -0,0 +1,19 @@
+#◽Index Operator []
+
+list1 = [1, 3, 5, 7, 9]
+
+list1[1] = 2    # writing
+
+x = list1[1]    # reading
+
+print(list1)    # Output: [1, 2, 5, 7, 9]
+
+print(x)        # Output: 2
+
+print()
+
+#◽Slice Operator [ : ]
+print(list1[:])     # Output: [1, 2, 5, 7, 9] --- if we pass only colon inside the square bracets[] it will print the entier list.
+
+print(list1[2:])    # Output: [5, 7, 9] --- Now here this will start from index 2 till last of the list
+
