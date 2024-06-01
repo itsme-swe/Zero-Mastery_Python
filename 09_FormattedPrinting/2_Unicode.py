3418a6b8816a68c326d74864be2776df9552dd22
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-01 08:01:47 +0530

    update

--- 09_FormattedPrinting/2_Unicode.py
+++ 09_FormattedPrinting/2_Unicode.py
@@ -4,3 +4,6 @@ print(s)    # Output: α  Here '\u' stands for unicode
 hindi = '\u0939\u097A\u0900'
 
 print(hindi)    # output: हॺऀ
+
+rupee = '\u20B9'
+print(rupee) #Output: ₹
---
+++ 09_FormattedPrinting/3_escapeSequence.py
@@ -0,0 +1,16 @@
+print("abc\fcde", end='')
+'''
+🔸Output: abc
+             cde
+
+'''
+
+print("abc\tdef", end='')
+
+print()
+
+rupee = '\N{rupee sign}'
+print(rupee)    # Output: ₨
+
+copy = '\N{copyright sign}'
+print(copy) # Output: ©
