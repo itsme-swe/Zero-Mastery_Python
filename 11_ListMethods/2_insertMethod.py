1d3c653542eb1601dae3985cbd195a84075a36a1
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-01 22:48:22 +0530

    update

--- 11_ListMethods/2_insertMethod.py
+++ 11_ListMethods/2_insertMethod.py
@@ -7,3 +7,9 @@ l1.insert(1, 3)
 print(l1)   # output: [2, 3, 4, 6, 8, 10]
 
 #🔸Syntax -- list.insert(indexVal, element)
+
+print()
+
+l2 = l1.copy()      # "copy()" method creates shallow list means it give us new list  
+
+print("l2: ",l2)    # Output: l2:  [2, 3, 4, 6, 8, 10]
