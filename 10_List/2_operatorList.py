6fa7691c6e87045f13eaf5abee8fbedf73132f14
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-01 13:06:12 +0530

    update

--- 10_List/2_operatorList.py
+++ 10_List/2_operatorList.py
@@ -1,6 +1,6 @@
 #◽Index Operator []
 
-list1 = [1, 3, 5, 7, 9]
+list1 = [1, 3, 5, 7, 9, 11, 13, 15]
 
 list1[1] = 2    # writing
 
@@ -17,3 +17,8 @@ print(list1[:])     # Output: [1, 2, 5, 7, 9] --- if we pass only colon inside t
 
 print(list1[2:])    # Output: [5, 7, 9] --- Now here this will start from index 2 till last of the list
 
+list1[4:8] = [12, 14, 16, 18]
+
+print(list1)    # Output: [1, 2, 5, 7, 12, 14, 16, 18] ---- We can modify the list with the help of slie operator just pass the startIndex and endIndex
+
+# Syntax -- list[startIndex, endIdex, step]
