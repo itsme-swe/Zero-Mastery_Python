b26665d111548b661e6b0423bdc3ff5718d34271
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-01 22:43:27 +0530

    update

---
+++ 11_ListMethods/2_insertMethod.py
@@ -0,0 +1,9 @@
+#◽insert() method used to add element at given index value
+
+l1 = [2, 4, 6 , 8, 10]
+
+l1.insert(1, 3)
+
+print(l1)   # output: [2, 3, 4, 6, 8, 10]
+
+#🔸Syntax -- list.insert(indexVal, element)
