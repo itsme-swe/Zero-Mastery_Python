d278e1ee80369376fa84112eaeb7def58e96f0d2
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-02 17:59:51 +0530

    update

--- 14_Tuple/1_tupleComprehension.py
+++ 14_Tuple/1_tupleComprehension.py
@@ -3,3 +3,9 @@
 t1 = tuple((x for x in range(1,11)))
 
 print(t1)   #Output: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+print()
+
+t2 = (*(x for x in range(1,11,2)),)
+
+print(t2)   # Output: (1, 3, 5, 7, 9)
