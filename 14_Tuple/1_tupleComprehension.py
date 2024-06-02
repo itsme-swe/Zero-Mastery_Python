09e18026b3473a1f3d7190b7d3b7bd7b3cbf851a
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-02 18:08:15 +0530

    update

--- 14_Tuple/1_tupleComprehension.py
+++ 14_Tuple/1_tupleComprehension.py
@@ -9,3 +9,9 @@ print()
 t2 = (*(x for x in range(1,11,2)),)
 
 print(t2)   # Output: (1, 3, 5, 7, 9)
+
+print()
+
+t3 = (*(x for x in 'PyThOn' if x.islower()),)
+
+print(t3)   #Output: ('y', 'h', 'n') only takes lowerCase letters
---
+++ 14_Tuple/2_tupleMethods.py
@@ -0,0 +1,7 @@
+# ◽Tuple is having only 2 methods ---- index() and count()
+
+t1 = (5, 3, 1, 4, 3, 1, 5)
+
+print(t1.count(5))      # Output: 2  This count() function work same as list count() function
+
+print(t1.index(3))      # Output 1 return the first occurence of element
